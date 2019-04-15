be2e3933765ae486e57f00139cd919be176fe02b
isaanca <isaanca@nuevaschool.org>
2019-04-15 12:43:16 -0700

    add dropdown menu (links to non-existent pages)

--- includeHTML.js
+++ includeHTML.js
@@ -2,13 +2,12 @@ function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
-  console.log(z);
+  // console.log(z);
   for (var i = z.length-1;  i >= 0;  i--) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("w3-include-html");
     if (file) {
-      console.log("Element found!")
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
