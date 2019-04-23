fc53d693e5186d6f97643e9df69cff8a69a32f04
isaanca <isaanca@nuevaschool.org>
2019-04-22 19:06:28 -0700

    Add image slideshow to events page, further organize files

---
+++ scripts/slideshow.js
@@ -0,0 +1,37 @@
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+
+  // slideshow restarts if it reaches the end
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  if (n <= -1) {
+    slideIndex = slides.length;
+  }
+
+  // hides all of the slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  // hides all of the dots/image selectors
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // shows the active slide and dot
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+}
